235c4024c3055cc495bf470639e254b67d96c545
carerit <kevnli2002@gmail.com>
2023-10-20 15:38:17 -0700

    added navbar toggle function, reworded about page, refactored home page v1

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,12 @@ import styles from "../page.module.css";
 export default function AboutPage() {
   return (
     <div className={styles.centeredDiv}>
-      <div className={styles.aboutBody}>Pawsome is a website that tests the DogCeo Api.</div>
+      <div className="container">
+        <h1>About Pawsome</h1>
+        <p className="lead">
+          I made Pawsome to demonstrate how to use Next.js to build a simple web app that uses an API. This app uses he dogCEO API to get random dog images. It also allows me to exercise 
+        </p>
+      </div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,56 +1,40 @@
-'use client';
+"use client";
 
 import styles from "./page.module.css";
-import React, { useRef, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
 export default function Home() {
-  const dogBoxRef = useRef(null);
 
-  useEffect(() => {
-    const button = document.querySelector(".btn-primary");
+  const [dogImage, setDogImage] = useState(
+    "https://dog.ceo/img/dog-api-logo.svg"
+  );
 
-    button.addEventListener("click", function () {
-      axios
-        .get("https://dog.ceo/api/breeds/image/random")
-        .then((response) => {
-          dogBoxRef.current.style.backgroundImage = `url(${response.data.message})`;
-        })
-        .catch((error) => console.log(error));
-    });
-  }, []);
+  function getRandomImage() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+      })
+      .catch((error) => console.error("Error fetching random image:", error));
+  }
 
   return (
     <div className="container">
       <div className={styles.tap}>
-        <button type="button" className="btn btn-primary">
-          Fetch!
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={getRandomImage}
+        >
+          Click for a random image
         </button>
         <h3 className={styles.headerText}>
           Change dog images by clicking the button!
         </h3>
+        <div className={styles.dogBox}>
+          <img id="dogImage" src={dogImage} alt="Random Dog" />
+        </div>
       </div>
-      <div ref={dogBoxRef} className={styles.dogBox}></div>
     </div>
   );
 }
-// const [dogImage, setDogImage] = useState(
-//   "https://dog.ceo/img/dog-api-logo.svg"
-// );
-
-// function getRandomImage() {
-//   fetch("https://dog.ceo/api/breeds/image/random")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setDogImage(data.message);
-//     })
-//     .catch((error) => console.error("Error fetching random image:", error));
-// }
-
-// return (
-//   <div className={styles.centeredDiv}>
-//     <Button onClick={getRandomImage}>Click for a random image</Button>
-
-//     <img id="dogImage" src={dogImage} alt="Random Dog" />
-//   </div>
-// );
